dcbae1613bb08f0a695ed5caf883bac25246042d
HeeWoong97 <ungung97@naver.com>
2020-06-02 23:29:13 +0900

    2020.06.02 23:29

--- boot/Main.c
+++ boot/Main.c
@@ -1,11 +1,15 @@
 #include "stdint.h"
 #include "HalUart.h"
 #include "HalInterrupt.h"
+#include "HalTimer.h"
 
 #include "stdio.h"
+#include "stdlib.h"
 
 static void Hw_init(void);
+
 static void Printf_test(void);
+static void Timer_test(void);
 
 void main(void) {
     Hw_init();
@@ -19,13 +23,15 @@ void main(void) {
     putstr("Hello World!\n");
     
     Printf_test();
-    
+    Timer_test();
+
     while(1);
 }
 
 static void Hw_init(void) {
     Hal_interrupt_init();
     Hal_uart_init();
+    Hal_timer_init();
 }
 
 static void Printf_test(void) {
@@ -41,4 +47,11 @@ static void Printf_test(void) {
     debug_printf("dec = %u, hex = %x\n", 0xff, 0xff);
     debug_printf("print_zero %u\n", 0);
     debug_printf("SYSCTRL0 %x\n", *sysctrl0);
+}
+
+static void Timer_test(void) {
+    while (1) {
+        debug_printf("current count : %u\n", Hal_timer_get_1ms_counter());
+        delay(1000);
+    }
 }
